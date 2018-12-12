467253ebb2d3f4ad9d5f4e708682fc54abd41105
EnriqueReina <44247185+EnriqueReina@users.noreply.github.com>
2018-12-12 14:25:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include "Matrix.h"
 
-
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
-
 int main(int argc, char** argv) 
 {
 	Matrix *m = new Matrix(3,4,true);
@@ -34,6 +28,33 @@ int main(int argc, char** argv)
 
 	m->addMatrix(*m2);
 	m->print();
+	
+	std::cout<<"==========================="<<std::endl;
+	
+	std::cout<<"\nMatriz resta a m, m2: "<<std::endl;
+
+	m->subtractMatrix(*m2);
+	m->subtractMatrix(*m2);
+	m->print();
+	
+	std::cout<<"==========================="<<std::endl;
+	
+	std::cout<<"\nMatriz suma a m, m2: "<<std::endl;
+
+	m->multiply(2.0);
+	m->print();
+	
+	std::cout<<"==========================="<<std::endl;
+	
+	std::cout<<"\nMatriz producto escalar de m3 con m4: "<<std::endl;
+
+	Matrix *m3 = new Matrix(3,3,true);
+	Matrix *m4 = new Matrix(3,4,true);
+	
+	Matrix *mdt = m3->dotProduct(*m4);
+	
+	mdt->print();
+	
 	
 	return 0;	
 
