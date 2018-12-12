483b73a25bdd49143c830e897f5b45a31e292a88
EnriqueReina <44247185+EnriqueReina@users.noreply.github.com>
2018-12-12 14:27:08 +0100

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -9,18 +9,29 @@ class Matrix
 {
 	public:
 		
+		//constructors
 		Matrix();
 		Matrix(std::vector < std::vector<double> > vals);
 		Matrix(int r, int c, bool rand);
 
-		double getValue(int r, int c);
+		//getters
+		double getValue(int r, int c){return m_vals.at(r).at(c);}
 		int getNumCol(){return this->m_y;}
 		int getNumRow(){return this->m_x;}
 
+		//setters
 		void setValue(int r, int c, double val);
 		
+		
+		//operation
 		void* addMatrix(Matrix m);
+		void* subtractMatrix(Matrix m);
+		void* multiply(double x);
+		Matrix* dotProduct(Matrix m);
 		Matrix* transpose();
+		
+		
+		//print
 		void print();
 
 	private:
